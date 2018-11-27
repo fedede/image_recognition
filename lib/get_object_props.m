59cb027132c09904f93c9558f7f989721668113c
Fede <fede@cocopifood.com>
2018-11-27 15:52:44 +0100

    Feature visualization

--- lib/get_object_props.m
+++ lib/get_object_props.m
@@ -1,12 +1,17 @@
-function [area,perimeter] = get_object_props(mask)
+function [area,perimeter,box_area,box_perimeter] = get_object_props(mask)
 %UNTITLED2 Summary of this function goes here
 %   Detailed explanation goes here
-props = regionprops(mask, 'FilledArea','Perimeter');
+props = regionprops(mask, 'FilledArea','Perimeter', 'BoundingBox');
 area = 0;
 perimeter = 0;
+box_area = 0;
+box_perimeter = 0;
 for i=1:size(props,1)
     area = area + props(i).FilledArea;
     perimeter = perimeter + props(i).Perimeter;
+    box_area = box_area + props(i).BoundingBox(3) * props(i).BoundingBox(4);
+    box_perimeter = box_perimeter + 2 * props(i).BoundingBox(3) + 2 * props(i).BoundingBox(4);
+    
 end
 end
 
