0b60d6b168500736897c7e0e09b13e292d54f925
Mateusz <nologin90@gmail.com>
2018-12-07 15:39:33 +0100

    extra features and some docs

--- lib/check_normalization.m
+++ lib/check_normalization.m
@@ -1,7 +1,7 @@
 function [correct] = check_normalization(features_n)
 %check_normalization Checks that normalization was correctly implemented
 m = sum(mean(features_n,1));
-s = sum(std(features_n)) / 3;
+s = sum(std(features_n)) / 6;
 mean_holds = m < 1e-6;
 std_holds = s < 1.0001 && s > 0.999;
 correct = mean_holds && std_holds;
